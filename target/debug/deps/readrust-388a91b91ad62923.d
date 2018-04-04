a3898a350385654a2a2abbd67d7029fe46f25484
Shreyas Lad <shadowtemplates@gmail.com>
2018-04-04 15:20:38 -0700

    hdfuishf

---
+++ target/debug/deps/readrust-388a91b91ad62923.d
@@ -0,0 +1,6 @@
+/Users/shreyaslad/readrust/target/debug/deps/readrust-388a91b91ad62923: src/main.rs src/about.rs
+
+/Users/shreyaslad/readrust/target/debug/deps/readrust-388a91b91ad62923.d: src/main.rs src/about.rs
+
+src/main.rs:
+src/about.rs:
